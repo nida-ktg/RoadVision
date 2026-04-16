7cea3733236f1623adc14a28c31c2ba4e7873049
Nida Aktug <nidaktug@gmail.com>
2026-04-16 15:38:35 +0300

    YOLO ile video nesne tespiti eklendi

--- src/detect_video.py
+++ src/detect_video.py
@@ -1,4 +1,4 @@
-"""
+'''
 from ultralytics import YOLO
 import cv2
 
@@ -6,10 +6,9 @@ import cv2
 model = YOLO("yolov8n.pt")
 
 # video aç
-cap = cv2.VideoCapture("videos/test_video.mp4")
+cap = cv2.VideoCapture("videos/video_2.mp4")
 
 while True:
-
     ret, frame = cap.read()
 
     if not ret:
@@ -18,15 +17,50 @@ while True:
     # nesne tespiti
     results = model(frame)
 
-    # sonuçları çiz
+    # sonucu çiz
     annotated_frame = results[0].plot()
 
     # ekranda göster
-    cv2.imshow("Detection", annotated_frame)
+    cv2.imshow("YOLO Detection", annotated_frame)
 
+    # q'ya basınca çık
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
 cv2.destroyAllWindows()
-"""
+'''
+
+from ultralytics import YOLO
+import cv2
+import os
+
+model = YOLO("yolov8n.pt")
+
+video_folder = "videos"
+
+for video_name in os.listdir(video_folder):
+
+    video_path = os.path.join(video_folder, video_name)
+
+    cap = cv2.VideoCapture(video_path)
+
+    print(f"Şu video işleniyor: {video_name}")
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        results = model(frame)
+        annotated_frame = results[0].plot()
+
+        cv2.imshow("YOLO Detection", annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+
+cv2.destroyAllWindows()
